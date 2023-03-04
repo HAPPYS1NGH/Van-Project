e98c6a1db2584fb96bf3dcf288dc9fb36a44b635
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-03-04 22:09:57 +0530

    Adjusted pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import {BrowserRouter , Routes , Route , Link} from "react-router-dom"
-import Home from './components/Home'
-import About  from "./components/About";
-import Vans from './components/Vans'
-import VanDetail from "./components/VanDetail";
+import Home from './pages/Home'
+import About  from "./pages/About";
+import Vans from './pages/Vans'
+import VanDetail from "./pages/VanDetail";
 import './App.css';
 import "./server"
 
